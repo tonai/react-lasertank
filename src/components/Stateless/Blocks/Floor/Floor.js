8b6d4ded59a2e649c82eea6b960936c0817942db
Tony CABAYE <tonai59+github@gmail.com>
2018-01-20 14:00:46 +0100

    Rename empty block => floor block

---
+++ src/components/Stateless/Blocks/Floor/Floor.js
@@ -0,0 +1,36 @@
+import React, { PureComponent } from 'react';
+
+import { getBottomStyles } from '../../../../services/styles';
+import { setTopTexture } from '../../../../services/texture';
+
+class Floor extends PureComponent {
+
+  drawTop = (canvas) => {
+    const { name, settings } = this.props;
+    setTopTexture(canvas, name, settings, this.props);
+  };
+
+  render() {
+    const { bottom, size, styles } = this.props;
+
+    return (
+      <div className="Floor pos-abs" style={styles}>
+        {!bottom && (<canvas
+          className="Water__side side--bottom pos-abs"
+          width={size}
+          height={size}
+          ref={this.drawTop}
+          style={getBottomStyles(size)}
+        />)}
+      </div>
+    );
+  }
+
+}
+
+Floor.defaultProps = {
+  x: 0,
+  y: 0,
+};
+
+export default Floor;
--- src/settings/blocks.js
+++ src/settings/blocks.js
@@ -1,23 +1,15 @@
-import boardSettings from './board';
 import generalSettings from './general';
 
 import Floor from '../components/Stateless/Blocks/Floor/Floor';
 import Water from '../components/Stateless/Blocks/Water/Water';
 import Block from '../components/Stateless/Blocks/Block/Block';
-import Empty from '../components/Stateless/Blocks/Empty/Empty';
 
 export default {
   floor: {
     component: Floor,
     spritePath: `${generalSettings.spritesDir}Inside_A4.png`,
     spriteOffset: [0, 0],
-    topStyles: {
-      backgroundPosition: `0px -${boardSettings.size / 2}px`
-    },
-    sideStyles: {
-      backgroundPosition: `0px -${boardSettings.size * 3 / 2}px`
-    },
-    bottomStyles: {}
+    spriteTopConnect: false
   },
   water: {
     component: Water,
@@ -30,15 +22,5 @@ export default {
     spritePath: `${generalSettings.spritesDir}Inside_A4.png`,
     spriteOffset: [0, 0],
     spriteTopConnect: false
-  },
-  empty: {
-    component: Empty,
-    spritePath: `${generalSettings.spritesDir}Inside_A4.png`,
-    spriteOffset: [0, 0],
-    bottomStyles: {
-      topStyles: {},
-      sideStyles: {},
-      backgroundPosition: `0px -${boardSettings.size / 2}px`
-    }
   }
 };
