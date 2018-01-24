589dfe611d7223edd63b9191e55c8d5dc1500741
Tony CABAYE <tony.cabaye@smile.fr>
2018-01-24 18:38:39 +0100

    Avoid two different textures to connect.

--- src/services/board.js
+++ src/services/board.js
@@ -1,12 +1,31 @@
 import rawMap from '../maps/test.json';
+
+import insert from 'ramda/es/insert';
+import join from 'ramda/es/join';
+import map from 'ramda/es/map';
 import mergeDeepRight from 'ramda/es/mergeDeepRight';
+import pipe from 'ramda/es/pipe';
+import prop from 'ramda/es/prop';
 import unnest from 'ramda/es/unnest';
+import values from 'ramda/es/values';
 
 function maxLength(acc, array) {
   return Math.max(acc, array.length);
 }
 
-function getComponentName(board, z, x, y) {
+function getComponentName(name, settings) {
+  if (settings.sprites) {
+    return pipe(
+      values,
+      map(sprite => `${sprite.path}_${sprite.offset}`),
+      insert(0, name),
+      join('-')
+    )(settings.sprites);
+  }
+  return name;
+}
+
+function getBoardComponentName(board, z, x, y) {
   return board[z] && board[z][x] && board[z][x][y] && board[z][x][y].props.name;
 }
 
@@ -40,15 +59,16 @@ export function getBlock(cell, settings, size, x, y, z) {
     return null;
   }
   const componentSettings = settings[cell[0]];
+  const mergedSettings = cell[1] ? mergeDeepRight(componentSettings, cell[1]) : componentSettings;
   return {
     component: componentSettings && componentSettings.component,
     props: {
-      name: cell[0],
+      name: getComponentName(cell[0], mergedSettings),
       x,
       y,
       z,
       size,
-      settings: cell[1] ? mergeDeepRight(componentSettings, cell[1]) : componentSettings,
+      settings: mergedSettings,
       styles: {transform: `translateX(${x * size}px) translateY(${y * size}px) translateZ(${z * size}px)`}
     }
   }
@@ -56,31 +76,31 @@ export function getBlock(cell, settings, size, x, y, z) {
 
 export function addAdjacentProps(board, x, y, z) {
   return {
-    bottom: getComponentName(board, z - 1, x, y),
-    bottomBack: getComponentName(board, z - 1, x - 1, y),
-    bottomBackLeft: getComponentName(board, z - 1, x - 1, y - 1),
-    bottomBackRight: getComponentName(board, z - 1, x - 1, y + 1),
-    bottomFront: getComponentName(board, z - 1, x + 1, y),
-    bottomFrontLeft: getComponentName(board, z - 1, x + 1, y - 1),
-    bottomFrontRight: getComponentName(board, z - 1, x + 1, y + 1),
-    bottomLeft: getComponentName(board, z - 1, x, y - 1),
-    bottomRight: getComponentName(board, z - 1, x, y + 1),
-    top: getComponentName(board, z + 1, x, y),
-    topBack: getComponentName(board, z + 1, x - 1, y),
-    topBackLeft: getComponentName(board, z + 1, x - 1, y - 1),
-    topBackRight: getComponentName(board, z + 1, x - 1, y + 1),
-    topFront: getComponentName(board, z + 1, x + 1, y),
-    topFrontLeft: getComponentName(board, z + 1, x + 1, y - 1),
-    topFrontRight: getComponentName(board, z + 1, x + 1, y + 1),
-    topLeft: getComponentName(board, z + 1, x, y - 1),
-    topRight: getComponentName(board, z + 1, x, y + 1),
-    back: getComponentName(board, z, x - 1, y),
-    backLeft: getComponentName(board, z, x - 1, y - 1),
-    backRight: getComponentName(board, z, x - 1, y + 1),
-    front: getComponentName(board, z, x + 1, y),
-    frontLeft: getComponentName(board, z, x + 1, y - 1),
-    frontRight: getComponentName(board, z, x + 1, y + 1),
-    left: getComponentName(board, z, x, y - 1),
-    right: getComponentName(board, z, x, y + 1)
+    bottom: getBoardComponentName(board, z - 1, x, y),
+    bottomBack: getBoardComponentName(board, z - 1, x - 1, y),
+    bottomBackLeft: getBoardComponentName(board, z - 1, x - 1, y - 1),
+    bottomBackRight: getBoardComponentName(board, z - 1, x - 1, y + 1),
+    bottomFront: getBoardComponentName(board, z - 1, x + 1, y),
+    bottomFrontLeft: getBoardComponentName(board, z - 1, x + 1, y - 1),
+    bottomFrontRight: getBoardComponentName(board, z - 1, x + 1, y + 1),
+    bottomLeft: getBoardComponentName(board, z - 1, x, y - 1),
+    bottomRight: getBoardComponentName(board, z - 1, x, y + 1),
+    top: getBoardComponentName(board, z + 1, x, y),
+    topBack: getBoardComponentName(board, z + 1, x - 1, y),
+    topBackLeft: getBoardComponentName(board, z + 1, x - 1, y - 1),
+    topBackRight: getBoardComponentName(board, z + 1, x - 1, y + 1),
+    topFront: getBoardComponentName(board, z + 1, x + 1, y),
+    topFrontLeft: getBoardComponentName(board, z + 1, x + 1, y - 1),
+    topFrontRight: getBoardComponentName(board, z + 1, x + 1, y + 1),
+    topLeft: getBoardComponentName(board, z + 1, x, y - 1),
+    topRight: getBoardComponentName(board, z + 1, x, y + 1),
+    back: getBoardComponentName(board, z, x - 1, y),
+    backLeft: getBoardComponentName(board, z, x - 1, y - 1),
+    backRight: getBoardComponentName(board, z, x - 1, y + 1),
+    front: getBoardComponentName(board, z, x + 1, y),
+    frontLeft: getBoardComponentName(board, z, x + 1, y - 1),
+    frontRight: getBoardComponentName(board, z, x + 1, y + 1),
+    left: getBoardComponentName(board, z, x, y - 1),
+    right: getBoardComponentName(board, z, x, y + 1)
   };
 }
